f3aeea4708717ef892096cc299d672d29f896ff6
thiago <thiagohissa@gmail.com>
2021-02-05 13:22:04 -0500

    basic structure setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-//import Employees from './components/Employees';
-import EmployeeList from './components/EmployeeList';
-import AddEmployee from "./components/AddEmployee";
-import EditEmployee from "./components/EditEmployee";
-import ViewEmployee from "./components/ViewEmployee"
-import Header from "./components/Header";
+import Landing from './components/Landing';
+
 
 
 function App() {
   return (
-    
-    <BrowserRouter>
-      <Header />
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={EmployeeList} />
-          <Route path="/add" component={AddEmployee} />
-          <Route path="/edit/:id" component={EditEmployee} />
-          <Route path="/view/:id" component={ViewEmployee} />
-        </Switch>
-      </div>
-
-      </BrowserRouter>
+    <Landing/>
   );
 }
 
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-const dotenv = require('dotenv')
-const result = dotenv.config()
-console.log(result.parsed)
-
+const dotenv = require('dotenv');
+const result = dotenv.config();
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const studentRoute = require('./routes/studentRoute');
 const app = express();
-
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
-});
-
 const port = 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+// Config Log
+console.log(result.parsed);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(studentRoute);
+
+// MongoDB Connection
+const DB_URL = "mongodb+srv://thiago:mypassword@cluster0.siwfc.mongodb.net/group-maker?retryWrites=true&w=majority";
+mongoose.Promise = global.Promise;
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database mongoDB Atlas Server");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// Server
+const server = app.listen(port, () => `Server running on port ${port}`);
