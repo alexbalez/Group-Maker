4a942f5aa98055abfc9c607a74bf7bd585ac1b28
Barrington <barrven@users.noreply.github.com>
2021-02-09 14:12:17 -0500

    Built footer component. Closes issue #55

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AuthDataConnector from "../services/AuthDataConnector";
+import Header from './Header'
 
 class Login extends Component{
     constructor(props){
@@ -34,32 +35,35 @@ class Login extends Component{
 
     render() {
         return (
-            <div className="container mt-5">
-                <div className="card col-md-6 offset-md-3 offset-md-3">
-                    <h3 className="text-center mt-3">Login</h3>
-                    <div className="card-body">
-                        <form>
-                            <div className="form-group">
-                                <label>Email</label>
-                                <input placeholder="Email" name="email" className="form-control"
-                                       ref={this.state.email}/>
-                            </div>
+            <div>
+                <Header/>
+                <div className="container mt-5">
+                    <div className="card col-md-6 offset-md-3 offset-md-3">
+                        <h3 className="text-center mt-3">Login</h3>
+                        <div className="card-body">
+                            <form>
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input placeholder="Email" name="email" className="form-control"
+                                        ref={this.state.email} />
+                                </div>
 
-                            <div className="form-group">
-                                <label>Password</label>
-                                <input placeholder="Password" name="password" type="password" className="form-control"
-                                       ref={this.state.password}/>
-                            </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input placeholder="Password" name="password" type="password" className="form-control"
+                                        ref={this.state.password} />
+                                </div>
 
-                            <div className="form-group">
-                                <span className="mr-3">Don't have an account?</span>
-                                <button className="btn btn-link" onClick={this.signup}>Sign Up</button>
-                            </div>
+                                <div className="form-group">
+                                    <span className="mr-3">Don't have an account?</span>
+                                    <button className="btn btn-link" onClick={this.signup}>Sign Up</button>
+                                </div>
 
-                            <div className="form-group text-center">
-                                <button className="btn btn-success btn-block" onClick={this.submit}>Login</button>
-                            </div>
-                        </form>
+                                <div className="form-group text-center">
+                                    <button className="btn btn-success btn-block" onClick={this.submit}>Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/components/student/Dashboard.js
+++ client/src/components/student/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../components.css';
 import Navigation from '../Navigation'
 import Header from '../Header'
-
+import Footer from '../Footer'
 
 class Dashboard extends Component {
     // constructor(props){
@@ -17,6 +17,10 @@ class Dashboard extends Component {
             <div>
                 <Header/>
                 <Navigation/>
+                    <div style={{height: "300px"}} className="m-5">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                <Footer/>
             </div>
         );
     }
