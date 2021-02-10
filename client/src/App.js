09d1d2e3a2f698324d4b0ce74acca98d73dfbc90
Barrington <barrven@users.noreply.github.com>
2021-02-10 16:50:11 -0500

    Added stubs in student components for Create, Find, Auto, help, and Profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,11 @@ import Landing from './components/Landing';
 import Login from './components/Login'
 import CreateAccount from './components/CreateAccount'
 import Dashboard from './components/student/Dashboard'
-
+import CreateGroup from "./components/student/CreateGroup";
+import FindGroup from "./components/student/FindGroup";
+import AutoGroup from "./components/student/AutoGroup";
+import HelpStudent from "./components/student/HelpStudent";
+import StudentProfile from "./components/student/StudentProfile";
 
 
 
@@ -18,6 +22,11 @@ function App() {
           <Route path='/signup' component={ CreateAccount } />
           <Route path="/mock" component={ Landing } />
           <Route path="/dashboard" component={ Dashboard } />
+          <Route path="/create" component={ CreateGroup }/>
+          <Route path="/find" component={ FindGroup } />
+          <Route path="/auto" component={ AutoGroup } />
+          <Route path="/help" component={ HelpStudent } />
+          <Route path="/profile" component={ StudentProfile } />
         </Switch>
       </BrowserRouter>
 
