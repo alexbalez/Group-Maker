37b1c3c7211cab76dae41898c3cfade36be3106d
KevinUfkes <kevinufkes@gmail.com>
2021-02-11 14:54:43 -0800

    added ProjectModel.js, Closes #71

---
+++ model/ProjectModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    assigneddate: {
+        type: Date
+    },
+    duedate: {
+        type: Date
+    },
+    weight: {
+        type: Number,
+        required: true,
+        trim: true, 
+    },
+    mingroupsize: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    maxgroupsize: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "campuses"
+    },
+    groups: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "groups"
+        }
+    ],
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "users"
+        }
+    ]
+})
+
+const Project = mongoose.model("Projects", ProjectSchema);
+module.exports = Project;
