dae31249240ea17bf8a2f124979f522e965fa319
KevinUfkes <kevinufkes@gmail.com>
2021-02-18 13:29:10 -0800

    added UserPreferenceRoutes.js, tested with Postman, Closes #86

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const preferenceRouter = require('./routes/crud_routes/PreferenceRoutes.js')
 
 const userRoleRouter = require('./routes/entity_relationship_routes/UserRoleRoutes.js')
 const userGroupRouter = require('./routes/entity_relationship_routes/UserGroupRoutes.js')
+const userPreferenceRouter = require('./routes/entity_relationship_routes/UserPreferenceRoutes.js')
 
 
 
@@ -48,6 +49,7 @@ app.use(preferenceRouter);
 
 app.use(userRoleRouter);
 app.use(userGroupRouter);
+app.use(userPreferenceRouter);
 
 
 
