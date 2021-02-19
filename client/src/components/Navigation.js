fecc205dda6c63dc8163af0bf5f1a0c9571ce7ec
alexbalez <alexbalez@gmail.com>
2021-02-19 16:47:30 -0500

    temporarily removed history

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,7 +6,9 @@ import profilePhoto from '../img/avatar-placeholder.gif'
 class Navigation extends Component {
     constructor(props){
         super(props);
-        this.state = {
+        this.location = props.location
+        this.state = {            
+            data: this.props.data,
             username: 'user'
         }
 
@@ -15,14 +17,20 @@ class Navigation extends Component {
             console.log("session pretend destroyed")
             //must pass the history from parent component
             //https://stackoverflow.com/questions/43837212/this-props-history-push-works-in-some-components-and-not-others
-            this.props.history.push("/")
+            //this.props.history.push("/")
             
         }
     }
 
+    componentWillMount() {
+        //this.setState({ data: nextProps.data });
+    }
+
+
     componentDidMount() {
         //todo: pull in information here specific to each user
         this.setState({username: 'Joeseph Username'})
+        console.log(this.props)
     }
 
     render() {
@@ -37,7 +45,7 @@ class Navigation extends Component {
                         <Nav.Link active={this.props.active === "auto"} href="/auto">Auto</Nav.Link>
                         <Nav.Link active={this.props.active === "help"} href="/help">Help</Nav.Link>
                     </Nav>
-
+                    
                     <Nav className="ml-auto">
                     <img src={profilePhoto} alt="Profile" width="50" height="50"/>
                         <NavDropdown title={this.state.username} id="basic-nav-dropdown">
