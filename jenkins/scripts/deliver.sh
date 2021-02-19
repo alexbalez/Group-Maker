e98351ce975b9b8490580036238a3f8af0f6ea75
Alex Balez <alexbalez@gmail.com>
2021-02-19 12:41:38 -0500

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-cp ~/Group-Maker/client/certs/joina.group.crt ./client/certs/joina.group.crt
-cp ~/Group-Maker/client/certs/joina.group.key ./client/certs/joina.group.key
+cp /home/ubuntu/Group-Maker/client/certs/joina.group.crt ./client/certs/joina.group.crt
+cp /home/ubuntu/Group-Maker/client/certs/joina.group.key ./client/certs/joina.group.key
 npm run dev
 sleep 1
 echo $! > .pidfile
