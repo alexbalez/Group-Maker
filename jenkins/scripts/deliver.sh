c6aa5fcb88b151a16c0e02e2359572ae0d38be8d
Alex Balez <alexbalez@gmail.com>
2021-02-19 12:34:52 -0500

    removed sudos

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-sudo cp /etc/letsencrypt/live/joina.group/joina.group.crt ./client/certs/joina.group.crt
-sudo cp /etc/letsencrypt/live/joina.group/joina.group.key ./client/certs/joina.group.key
+cp /etc/letsencrypt/live/joina.group/joina.group.crt ./client/certs/joina.group.crt
+cp /etc/letsencrypt/live/joina.group/joina.group.key ./client/certs/joina.group.key
 npm run dev
 sleep 1
 echo $! > .pidfile
