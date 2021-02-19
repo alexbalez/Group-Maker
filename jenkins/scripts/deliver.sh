5de8e53aef91644231eda3e9d127dcef8718c78e
Alex Balez <alexbalez@gmail.com>
2021-02-19 12:38:46 -0500

    certs out of /etc

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-cp /etc/letsencrypt/live/joina.group/joina.group.crt ./client/certs/joina.group.crt
-cp /etc/letsencrypt/live/joina.group/joina.group.key ./client/certs/joina.group.key
+cp ~/Group-Maker/client/certs/joina.group.crt ./client/certs/joina.group.crt
+cp ~/Group-Maker/client/certs/joina.group.key ./client/certs/joina.group.key
 npm run dev
 sleep 1
 echo $! > .pidfile
