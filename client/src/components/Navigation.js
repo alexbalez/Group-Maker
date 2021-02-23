03c0b805e5b70b0e03a5f18f7d00b3b02f18792c
alexbalez <alexbalez@gmail.com>
2021-02-23 15:19:31 -0500

    display username instead of email

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -48,7 +48,7 @@ class Navigation extends Component {
                     </Nav>
                     
                     <Nav className="ml-auto">
-                        <NavDropdown className="bg-dark" title={this.state.data.email} id="nav-dropdown">
+                        <NavDropdown className="bg-dark" title={this.state.data.username} id="nav-dropdown">
                             <NavDropdown.Item onClick={ this.goToProfile }>My Profile</NavDropdown.Item>
                             <NavDropdown.Item onClick={ this.logout }>Logout</NavDropdown.Item>
                             
