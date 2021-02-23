e4cafa337f3f8bdf78f00490dd8b6c14c0ef03ce
Barrington <barrven@users.noreply.github.com>
2021-02-23 12:56:56 -0500

    Fixed up 404 page closes issue #134

--- client/src/components/404.js
+++ client/src/components/404.js
@@ -1,12 +1,18 @@
 import './components.css';
 import React, { Component } from "react";
+import {Link} from 'react-router-dom'
 
 class FourOhFour extends Component{
 
     render(){
         return (
             <div>
-                404 :(
+                <div style={{ height: "300px" }} className="m-5">
+                    <h1>404 Not Found</h1>
+                    <p>The Page you requested does not exist</p>
+                    <Link to="/dashboard">Back to Dashboard</Link>
+                </div>
+
             </div>
         )
     }
