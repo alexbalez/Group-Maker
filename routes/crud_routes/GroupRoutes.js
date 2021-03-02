81014658f2558324b166ff758562eff4dd1e47b1
alexbalez <alexbalez@gmail.com>
2021-03-02 16:31:15 -0500

    added basic search route

--- routes/crud_routes/GroupRoutes.js
+++ routes/crud_routes/GroupRoutes.js
@@ -27,6 +27,24 @@ app.get('/groups', async (req, res) => {
   }
 });
 
+//Get groups containing :name (no case)
+app.get('/groups/:name/', async(req,res) => {
+  const group = await groupModel.find({
+    'name': { $regex: req.params.name, $options:'i' }
+  }, 'id name description').exec((err, search) => {
+    console.log(err)
+    if (err) res.status(500).send(err)
+    console.log(search)
+  })
+  try {
+      res.append('Access-Control-Allow-Origin', ['*']); 
+      res.send(group);
+  }
+  catch (err) {
+      res.status(500).send(err);
+  }
+});
+
 app.get('/group/:id', async(req,res) => {
     console.log(req.params.id)
     const group = await groupModel.findById(req.params.id);
