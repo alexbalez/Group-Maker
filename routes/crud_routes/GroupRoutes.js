468297fddc6c92ef11441c0ff8e786a62e583e3d
alexbalez <alexbalez@gmail.com>
2021-03-03 11:37:23 -0500

    better search

--- routes/crud_routes/GroupRoutes.js
+++ routes/crud_routes/GroupRoutes.js
@@ -27,22 +27,14 @@ app.get('/groups', async (req, res) => {
   }
 });
 
-//Get groups containing :name (no case)
-app.get('/groups/:name/', async(req,res) => {
+//Search groups containing :name (no case)
+app.get('/groups/s/name/:name', async(req, res) => {
   const group = await groupModel.find({
     'name': { $regex: req.params.name, $options:'i' }
   }, 'id name description').exec((err, search) => {
-    console.log(err)
     if (err) res.status(500).send(err)
-    console.log(search)
+    res.send(search)
   })
-  try {
-      res.append('Access-Control-Allow-Origin', ['*']); 
-      res.send(group);
-  }
-  catch (err) {
-      res.status(500).send(err);
-  }
 });
 
 app.get('/group/:id', async(req,res) => {
