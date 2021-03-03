1a33b645220db9186f96216e0927ea72ba287bf0
alexbalez <alexbalez@gmail.com>
2021-03-03 13:39:13 -0500

    add group search

--- client/src/components/student/FindGroup.js
+++ client/src/components/student/FindGroup.js
@@ -2,19 +2,53 @@ import React, { Component } from 'react'
 import '../components.css'
 import {Button, Form, InputGroup, FormControl, Dropdown, Table, Navbar} from 'react-bootstrap';
 import JoinPopUp from '../JoinPopUp';
+import axios from 'axios'
+
 
 class FindGroup extends Component {
     constructor(props){
         super(props);
         this.state = {
-            popup: false
+            popup: false,
+            id: '',
+            search: '',
+            results: []
         }
     }
 
+    handleSearch = (e) => {
+        e.preventDefault()
+        axios.post('/groups/s/name', {
+            name: this.state.search
+        }).then((res) => {
+            console.log("result:",res)
+            this.setState({results: res.data})
+        }, (err) => {
+            console.log("err",err)
+        })
+    }
+
+    handleSearchChange = (e) => {
+        e.preventDefault()
+        this.setState({search: e.target.value})
+        console.log(this.state.search)
+    }
 
     handleJoin = (e) => {
         e.preventDefault();
+        this.setState({id: e.target.value})
         this.setState({popup: !this.state.popup});
+    }
+
+    handleSearchResults = (result, index) => {
+        console.log(result)
+        return(
+            <tr key={index}>
+                <td colSpan="2">{result.name}</td>
+                <td>{result.description}</td>
+                <td><Button variant="success" value={result._id} onClick={this.handleJoin}>Join</Button></td>
+            </tr>
+        )
     }
 
     render() {
@@ -22,7 +56,7 @@ class FindGroup extends Component {
             <div>
 
                 {/* Join PopUp - Fires when user clicks on "Join" */}
-                {this.state.popup ? <JoinPopUp text="Testing popup" closePopup={this.handleJoin}/> : null}
+                {this.state.popup ? <JoinPopUp id={this.state.id} closePopup={this.handleJoin}/> : null}
 
                 {/* Search Bar */}
                 <Navbar className="bg-light justify-content-center">
@@ -41,8 +75,8 @@ class FindGroup extends Component {
                             </Dropdown>
                         </InputGroup>
                     
-                        <FormControl type="text" placeholder="Search suggested" className="m-1" />
-                        <Button variant="success" type="submit" className="m-1">Search</Button>
+                        <FormControl type="text" placeholder="Search suggested" className="m-1" onChange={this.handleSearchChange} />
+                        <Button variant="success" type="submit" className="m-1" onClick={this.handleSearch}>Search</Button>
                         <Button className="m-1" variant="danger">Reset</Button>
                     
                     </Form>
@@ -60,46 +94,7 @@ class FindGroup extends Component {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td colSpan="2">T127 Study Group</td>
-                            <td>Course Group</td>
-                            <td><Button onClick={this.handleJoin} variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">COMP 1101 Assignment 1</td>
-                            <td>Assignment</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">COMP 1101 Assignment 1</td>
-                            <td>Assignment</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">T127 Study Group</td>
-                            <td>Course Group</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">COMP 1101 Assignment 1</td>
-                            <td>Assignment</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">COMP 1101 Assignment 1</td>
-                            <td>Assignment</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">COMP 2201 Assignment 2</td>
-                            <td>Assignment</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">COMP 2201 Assignment 1</td>
-                            <td>Assignment</td>
-                            <td><Button variant="success">Join</Button></td>
-                        </tr>
+                            {this.state.results.map(this.handleSearchResults)}
                         </tbody>
                     </Table>
                 </div>
