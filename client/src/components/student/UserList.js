acb7fbc4f44499ac9666df6a3f6b0bc0367fe18a
alexbalez <alexbalez@gmail.com>
2021-03-05 16:02:01 -0500

    list changes attempt

--- client/src/components/student/UserList.js
+++ client/src/components/student/UserList.js
@@ -8,28 +8,55 @@ class UserList extends Component{
         super(props)
         this.state = {
             users: props.users,
-            list: ""
+            list: [],
+            usersresolved: [],
+            resolved: false,
         }
     }
 
-    updateList = () => {
-        const users = this.state.users
-        const items = users.map((user) => 
-            <li key={user.toString()}>
-                {user.toString()}
-            </li>
-        )
-        this.setState({list: items})
+    resolveUsers = (users) => {
+        // ids -> usernames
+        const resolved = []
+        const promises = users.map((user) => {
+            axios.get('/user/'+user)
+            .then((res)=> {
+                resolved.push(res.data.username) // change later to prettify userlist? profile link?
+                return res.data.username
+            }, (err) => {
+                console.log(err)
+            })
+        })
+
+        //wait until all users have resolved, then update component
+        Promise.all(promises).then(()=>{
+            this.setState({usersresolved: resolved})
+            var items = this.state.usersresolved
+            console.log("users res:", this.state.usersresolved)
+            console.log('items',items)
+            const mappeditems = items.map(user => {
+                <li key={user.toString()}>
+                    {user.toString()}
+                </li>
+            })
+            console.log("after",mappeditems)
+            this.setState({list: mappeditems})     
+            this.setState({resolved: true})
+
+        })
     }
 
     componentDidMount(){
-        this.updateList()
+        if (!this.state.resolved){
+            this.resolveUsers(this.state.users)
+        }
     }
 
     render(){
-        return(
-            <ul>{this.state.list}</ul>
-        )
+
+            return(
+                <ul>{this.state.list}</ul>
+            )
+        
     }
 }
 
