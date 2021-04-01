33ab30d626cf3dce05bd0407a5639a12845db7aa
alexbalez <alexbalez@gmail.com>
2021-04-01 16:01:00 -0400

    moved copyright notice to page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-import './components.css';
-import { Container, Row, Col } from 'react-bootstrap'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./components.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 class Footer extends Component {
-    // constructor(props){
-    //     super(props);
-    // }
+  // constructor(props){
+  //     super(props);
+  // }
 
-    componentDidMount() {
-    }
+  componentDidMount() {}
 
-    render() {
-        return (
-
-            <Container fluid className="bg-primary text-white fixed-bottom" >
-                <Row className="p-2">
-                    <Col sm></Col>
-                    <Col sm className="text-center">&copy;2021 Group Maker</Col>
-                    <Col sm className="text-right">
-                        <Link className="text-white" to="/about">About</Link>
-                        &nbsp;| &nbsp;
-                        <Link className="text-white" to="/contact">Contact Us</Link>
-                    </Col>
-                </Row>
-            </Container>
-
-        );
-    }
-
-
+  render() {
+    return (
+      <Container fluid className="bg-primary text-white">
+        <Row className="p-2">
+          <Col sm></Col>
+          <Col sm className="text-center">
+            &copy;2021 Group Maker
+          </Col>
+          <Col sm className="text-right">
+            <Link className="text-white" to="/about">
+              About
+            </Link>
+            &nbsp;| &nbsp;
+            <Link className="text-white" to="/contact">
+              Contact Us
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Footer;
